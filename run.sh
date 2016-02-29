3b0767be12b0cd508da42bb9e25c9d02273de27d
Henry Heberle <henryhbrl@gmail.com>
2016-02-29 17:29:24 -0300

    Finalização da execução dos scripts em R para a ROmenia

--- run.sh
+++ run.sh
@@ -39,11 +39,11 @@ echo "-------------------------------------------"
 echo "Executing DoubleCV with NSC"
 # Run Double-Cross-Validation and Rankings-Validation using NSC
 R -f ./src/script_NSC_DoubleCrossValidation.R #>/dev/null
-R -f ./src/script_rank_validation_nsc_test_train.R #>/dev/null
+#R -f ./src/script_rank_validation_nsc_test_train.R #>/dev/null
 echo "-------------------------------------------"
 echo " "
 echo "-------------------------------------------"
 echo "Executing DoubleCV with SVM-RFE"
 # Run Double-Cross-Validation and Rankings-Validation using SVM-RFE and SVM
 R -f ./src/script_SVM_RFE_DoubleCrossValidation.R #>/dev/null
-R -f ./src/script_rank_validation_svmrfe_test_train.R #>/dev/null
+#R -f ./src/script_rank_validation_svmrfe_test_train.R #>/dev/null
